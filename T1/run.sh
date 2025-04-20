c8c9d196b3d9d5f55105157a5707ffb623b43efd
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2025-04-20 17:58:14 -0300

    update bash

--- T1/run.sh
+++ T1/run.sh
@@ -1,1 +1,3 @@
-java -jar /home/guhonda/Projects/COMPILADORES/corretor/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar "java -jar /home/guhonda/Projects/COMPILADORES/T1/alguma-lexico/target/alguma-lexico-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc /home/guhonda/Projects/COMPILADORES/corretor/temp /home/guhonda/Projects/COMPILADORES/corretor/casos-de-teste "811716, 811773, 812287" t1
+cd ./alguma-lexico
+mvn clean package
+java -jar ../../corretor/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar "java -jar ./target/alguma-lexico-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc ../../corretor/temp ../../corretor/casos-de-teste "811716, 811773, 812287" t1
