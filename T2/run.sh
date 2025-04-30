6e022a610418ad047189b0bae0c66811e36b5d88
Vitor Vechin <vitorvechin@gmail.com>
2025-04-30 16:02:03 -0300

    ajusta script

--- T2/run.sh
+++ T2/run.sh
@@ -1,3 +1,3 @@
-cd ./alguma-lexico
+cd ./alguma-sintatico
 mvn clean package
 java -jar ../../corretor/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar "java -jar ./target/alguma-sintatico-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc ../../corretor/temp ../../corretor/casos-de-teste "811716, 811773, 812287" t2
